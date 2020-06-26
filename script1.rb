52ebbe89cfe53d28837f429c40d3f235dfe91082
NikharMehta3 <59989603+NikharMehta3@users.noreply.github.com>
2020-06-26 13:49:45 -0400

    Update script1.rb

--- script1.rb
+++ script1.rb
@@ -1,5 +1,4 @@
 require "json"
 file = File.open "data.json"
 data = JSON.load file
-some_data = data["security"]["sastScan"]["enabled"]
-exit(some_data)
+STDOUT.puts data["security"]["sastScan"]["enabled"]
