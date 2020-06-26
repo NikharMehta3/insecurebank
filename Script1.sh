7535587c242a825a52d127da2765b618def43ce3
NikharMehta3 <59989603+NikharMehta3@users.noreply.github.com>
2020-06-26 14:25:14 -0400

    Update Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -24,7 +24,7 @@ echo $p
 echo $p >> result.json
 #chmod +x script1.rb
 
-var=ruby script1.rb
+ruby script1.rb
 echo $var
 
 #echo $(ruby -e \'require "json"; file = File.open "result.json"; data = JSON.load file; puts data["security"]["sastScan"]["enabled"]')
